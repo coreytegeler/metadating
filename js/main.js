1d60a5be2cee3345a434513e2f7a5b020b01a2f7
TEGELER <corey.tegeler@li1015-as03.purchase.edu>
2014-11-09 18:48:39 -0500

    trackin yo face;

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-  
-  fit();
+  webcam = document.getElementById('webcam');
+  overlay = $('#detectionOverlay');
+  canvas = document.getElementById('face');
+  ctx = canvas.getContext('2d');
+  initWebcam();
+  fitSections();
   var fromTop = 0;
   var completeCalled = false;
   $(window).on('mousewheel', function() {
@@ -9,13 +13,12 @@ $(document).ready(function() {
     if(fromTop >= 20) {
       $("html, body").animate({scrollTop: h()}, 400, function(){
         if(!completeCalled){
-            completeCalled = true;
-            alert('this alert will popup once');
+          completeCalled = true;
+          alert('this alert will popup once');
         }
       });
     }
   });
-
 
   $(window).scroll(function(event) {
     console.log(event);
@@ -30,17 +33,28 @@ $(document).ready(function() {
 });
 
 $(window).resize(function() {
-  fit();
+  fitSections();
 });
 
 $(window).on('beforeunload', function(){
   $(document).scrollTop(0);
 });
 
-function fit() {
+function fitSections() {
   $('section').each(function() {
     $(this).height(window.innerHeight);
   });
+}
+
+function fitCam() {
+  // webcamWidth = webcam.videoWidth;
+  // webcamHeight = webcam.videoHeight;
+  // webcamRatio = webcamWidth/webcamHeight;
+  // console.log(webcamWidth, webcamHeight, webcamRatio);
+  // overlay.width(window.innerWidth);
+  // overlay.height(Math.round(window.innerWidth / webcamRatio));
+  // var top = winW()/2 - overlay.height()/2;
+  // overlay.css({'top': top});
 }
 
 function catchHeader() {
@@ -51,8 +65,55 @@ function dropHeader() {
   $('header.fixed').removeClass('caught');
 }
 
-function h() {
+function winH() {
   return window.innerHeight;
+}
+
+function winW() {
+  return window.innerWidth;
+}
+
+function initWebcam() {
+    if (Modernizr.getusermedia) {
+      var userMedia = Modernizr.prefixed('getUserMedia', navigator);
+      userMedia({video:true}, function(localMediaStream) {
+        webcam.src = window.URL.createObjectURL(localMediaStream);
+        webcam.onloadedmetadata = function(e) {
+          fitCam()
+          $(window).resize(function() {
+            fitCam();
+          });
+          authorize();
+        };
+      }, function() {
+        console.log('Failed');
+      });
+    }
+  }
+
+function authorize() {
+  tracker = new clm.tracker();
+  tracker.init(pModel);
+  tracker.start(webcam);
+  positionLoop();
+  drawLoop()
+}
+
+function positionLoop() {
+  requestAnimationFrame(positionLoop);
+  var positions = tracker.getCurrentPosition();
+  positionString = "";
+  if(positions) {
+    for (var p=0;p<10;p++) {
+      positionString += positions[p][0].toFixed(2)+","+positions[p][1].toFixed(2);
+    }
+  }
+}
+
+function drawLoop() {
+  requestAnimationFrame(drawLoop);
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  tracker.draw(canvas);
 }
 
 
